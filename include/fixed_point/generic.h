3d2c2148d23fe89e0e39d434c8bf46b2d5d0c13f
JunKi(Dylan), Hong <dylanhong920509@gmail.com>
2026-01-11 19:21:04 +0900

    Improves fixed-point multiplication precision and usage
    Refactors fixed-point multiplication to use a wider data type for intermediate results, reducing overflow risk and improving accuracy. Introduces a new multiplication helper for extended types and updates bicubic interpolation logic to use the new helpers, ensuring correct type handling in arithmetic operations.

--- include/fixed_point/generic.h
+++ include/fixed_point/generic.h
@@ -9,22 +9,24 @@
 #define SGL_INT_TO_Q11(num)         ((num) << SGL_Q11_FRAC_BITS)
 #define SGL_Q11_GET_INT_PART(num)   ((num) >> SGL_Q11_FRAC_BITS)
 #define SGL_Q11_GET_FRAC_PART(num)  ((num) & SGL_Q11_MASK)
-#define SGL_Q11_ROUNDUP(num)        ((num) + (SGL_TYPEOF((num)))(1U << (SGL_Q11_FRAC_BITS - 1)))
+#define SGL_Q11_ROUNDUP(num)        ((num) + (SGL_TYPEOF((num)))(1 << (SGL_Q11_FRAC_BITS - 1)))
 #define SGL_Q11_SHIFTDOWN(num)      ((num) >> SGL_Q11_FRAC_BITS)
 
 typedef int16_t                     sgl_q11_t;
 typedef int32_t                     sgl_q11_ext_t;
 
-static SGL_ALWAYS_INLINE sgl_q11_t sgl_q11_mul(sgl_q11_t a, sgl_q11_t b) {
-    /* Avoid UB(Undefined Behavior) by changing 'signed' to 'unsigned'. */
-    uint32_t ua = (uint32_t)a;  /* Q11 */
-    uint32_t ub = (uint32_t)b;  /* Q11 */
-    uint32_t prod = ua * ub;    /* Q22 = Q11 x Q11 */
+static SGL_ALWAYS_INLINE sgl_q11_t sgl_q11_mul(sgl_q11_t a, sgl_q11_t b)
+{
+    sgl_q11_ext_t prod = (sgl_q11_ext_t)a * (sgl_q11_ext_t)b;   /* Q22 = Q11 x Q11 */
+    prod = SGL_Q11_ROUNDUP(prod);                               /* Rounding */
+    return (sgl_q11_t)SGL_Q11_SHIFTDOWN(prod);                  /* Q11 */
+}
 
-    /* Rounding */
-    prod = (uint32_t)SGL_Q11_ROUNDUP(prod);
-
-    return (sgl_q11_t)SGL_Q11_SHIFTDOWN(prod);  /* Q11 */
+static SGL_ALWAYS_INLINE sgl_q11_ext_t sgl_q11_ext_mul(sgl_q11_ext_t a, sgl_q11_ext_t b)
+{
+    sgl_q11_ext_t prod = a * b;     /* Q22 = Q11 x Q11 */
+    prod = SGL_Q11_ROUNDUP(prod);   /* Rounding */
+    return SGL_Q11_SHIFTDOWN(prod); /* Q11 */
 }
 
 static SGL_ALWAYS_INLINE uint8_t sgl_clamp_u8_i32(int32_t val)
